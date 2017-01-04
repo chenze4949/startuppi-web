dc9faf901f8683d0629e6a4ba7aaa3d72e41d89d
Ze Chen <saul@share.asia>
2017-01-04 09:16:01 +0800

    change https

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -6,5 +6,5 @@
 export var sep='/';
 export var version: string="22.2.2";
 
-export var host:string = "http://startuppi.herokuapp.com"
+export var host:string = "https://startuppi.herokuapp.com"
 // export var host:string = "http://10.0.1.117:3000"
