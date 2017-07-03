f038b38a8b3500f3a3d810264b0aebf772b94918
Paul Chan <chanzakbi@gmail.com>
2017-07-03 18:24:57 +0800

    change api server domain for testing

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,11 +1,13 @@
 //
-// ===== File globals.ts    
+// ===== File globals.ts
 //
 'use strict';
 
-export var sep='/';
-export var version: string="22.2.2";
+export var sep = '/';
+export var version : string = "22.2.2";
 
-export var host:string = "https://startuppi.herokuapp.com/api/v1"
-export var host_v:string = "https://startuppi.herokuapp.com/"
+// export var host:string = "https://startuppi.herokuapp.com/api/v1" export var
+// host_v:string = "https://startuppi.herokuapp.com/" testing
+export var host : string = "https://startuppi-testing.herokuapp.com/api/v1"
+export var host_v : string = "https://startuppi-testing.herokuapp.com/"
 // export var host:string = "http://10.0.1.117:3000"
