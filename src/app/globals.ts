0fb679789aa25d8f1deb88cecd3b7bb581d8e7af
Paul Chan <chanzakbi@gmail.com>
2017-07-03 18:29:01 +0800

    change domain for production

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -6,8 +6,9 @@
 export var sep = '/';
 export var version : string = "22.2.2";
 
-// export var host:string = "https://startuppi.herokuapp.com/api/v1" export var
-// host_v:string = "https://startuppi.herokuapp.com/" testing
-export var host : string = "https://startuppi-testing.herokuapp.com/api/v1"
-export var host_v : string = "https://startuppi-testing.herokuapp.com/"
-// export var host:string = "http://10.0.1.117:3000"
+export var host : string = "https://startuppi.herokuapp.com/api/v1"
+export var host_v : string = "https://startuppi.herokuapp.com/"
+// testing export var host : string =
+// "https://startuppi-testing.herokuapp.com/api/v1" export var host_v : string =
+// "https://startuppi-testing.herokuapp.com/" export var host:string =
+// "http://10.0.1.117:3000"
